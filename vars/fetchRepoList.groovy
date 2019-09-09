b59b66a85de587acb6118c1fb235cdd7c6ace7ea
Ankush Kumar Singh <50256940+SupremoAnkush@users.noreply.github.com>
2019-09-09 16:17:21 +0530

    Update fetchRepoList.groovy

--- vars/fetchRepoList.groovy
+++ vars/fetchRepoList.groovy
@@ -1,3 +1,3 @@
 def call(){
-    curl http://18.224.155.110:8081/nexus/service/local/repositories
+    sh 'curl -u admin:admin123 http://18.224.155.110:8081/nexus/service/local/repositories;'
     }
