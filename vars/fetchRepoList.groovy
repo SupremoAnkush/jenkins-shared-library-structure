5aaaa5c36a1b4325e9cf8fcbd4bcff3eca2271a5
Ankush Kumar Singh <50256940+SupremoAnkush@users.noreply.github.com>
2019-09-09 18:03:27 +0530

    Update fetchRepoList.groovy

--- vars/fetchRepoList.groovy
+++ vars/fetchRepoList.groovy
@@ -1,5 +1,10 @@
 def call(){
-    def proc= "curl -u admin:admin123 http://18.224.155.110:8081/nexus/service/local/repositories".execute()
+    def repoUrl= "http://18.224.155.110:8081/nexus/service/local/repositories"
+    /*def proc= "curl -u admin:admin123 http://18.224.155.110:8081/nexus/service/local/repositories".execute()
     Thread.start { System.err << proc.err }
-    proc.waitFor()
+    proc.waitFor()*/
+    
+    def response = "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${json}' http://admin:admin123@http://18.224.155.110:8081/nexus/service/local/repositories".execute().text
+
+    println response
     }
