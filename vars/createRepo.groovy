d5672b19d720ccdb8f668cbb843b2a080c7856b0
Ankush Kumar Singh <50256940+SupremoAnkush@users.noreply.github.com>
2019-09-09 15:04:47 +0530

    Update createRepo.groovy

--- vars/createRepo.groovy
+++ vars/createRepo.groovy
@@ -1,3 +1,14 @@
 def call(){
-    curl -H "Content-Type: application/json" -d @Tycoon_8872.json -u admin:admin123 'http://18.224.155.110:8081/nexus/service/local/repositories;'
+    curl -H "Content-Type: application/json" -d @repo.json -u admin:admin123 'http://18.224.155.110:8081/nexus/service/local/repositories';
+    "repositories": {
+      "repository": [
+        {
+          "create": true,
+          "repository_name": "Tycoon_8872",
+          "repository_type": "proxy",
+          "repository_policy": "RELEASE",
+          "repository_provider": "maven2",
+        }
+      ]
+    }
     }
