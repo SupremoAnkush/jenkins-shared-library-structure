9eaa53809b80e69f31aebd9a4ee4025904df1b94
Ankush Kumar Singh <50256940+SupremoAnkush@users.noreply.github.com>
2019-09-09 12:54:38 +0530

    Update createRepo.groovy

--- vars/createRepo.groovy
+++ vars/createRepo.groovy
@@ -1,3 +1,3 @@
 def call(){
-    curl -H "Content-Type: application/json" -d @Tycoon_8872.json -u admin:admin123 http://18.224.155.110:8081/nexus/service/local/repositories
+    curl -H "Content-Type: application/json" -d @Tycoon_8872.json -u admin:admin123 'http://18.224.155.110:8081/nexus/service/local/repositories'
     }
